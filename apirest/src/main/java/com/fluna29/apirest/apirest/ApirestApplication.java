c2fc06182944119f12da46514b319a0d2f4d5f3f
Fluna29 <fluna293107@gmail.com>
2024-09-06 10:13:11 +0200

    Hicimos una versión configurada del repositorio java-crud-api-rest pero con la configuración necesaria para subirlo a Railway

---
+++ apirest/src/main/java/com/fluna29/apirest/apirest/ApirestApplication.java
@@ -0,0 +1,15 @@
+package com.fluna29.apirest.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApirestApplication {
+
+    public static void main(String[] args) {
+        
+        // Inicia la aplicación Spring Boot
+        SpringApplication.run(ApirestApplication.class, args);
+    }
+}
---
+++ apirest/src/main/java/com/fluna29/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.fluna29.apirest.apirest.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fluna29.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+}
